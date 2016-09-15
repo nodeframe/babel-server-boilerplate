f276bca7c66ace24b04522996c9a4bd261f54662
Thakerng Soimadee <thakerng@Thakerngs-MacBook-Pro.local>
2016-09-15 18:07:26 +0700

    Init

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import {act,add} from 'utils/seneca';
-import {ROLE} from 'constants/role';
-import {ACTION} from 'constants/cmd';
 
-add({role:ROLE,action:ACTION},function({action}){
+add({role:"ROLE",action:"ACTION"},({action})=>{
   return {action};
 });
 
-act({role:ROLE,action:ACTION}).then((data)=>{
+act({role:"ROLE",action:"ACTION"}).then((data)=>{
   console.log(data);
 });
